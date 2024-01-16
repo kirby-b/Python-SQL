b58151f385ad45dff91f4c8eab30b2a0c272a1a5
Kirby-b <kirby.brodie66@gmail.com>
2024-01-16 09:23:15 -0500

    Create python_sql_add_tables.py

---
+++ python_sql_add_tables.py
@@ -0,0 +1,32 @@
+ import mysql.connector
+
+
+def main():
+    db = mysql.connector.connect(
+        host="localhost",
+        user="yourusername",
+        password="yourpassword",
+        database="thedatabase"
+    )   
+    dbcursor.execute("CREATE TABLE memes (name VARCHAR(255), score VARCHAR(255))")
+    sql = "INSERT INTO memes (name, score) VALUES (%s, %s)"
+    val = ("Rick Roll", "11/10")
+    dbcursor.execute(sql, val)
+    sql = "INSERT INTO memes (name, score) VALUES (%s, %s)"
+    val = ("Anything from vine", "10/10")
+    dbcursor.execute(sql, val)
+    sql = "INSERT INTO memes (name, score) VALUES (%s, %s)"
+    val = ("Bee Movie Script", "10/10")
+    dbcursor.execute(sql, val)
+
+    db.commit()
+
+    dbcursor.execute("SELECT * FROM memes")
+        
+    myresult = mycursor.fetchall()
+
+    for x in myresult:
+        print(x)
+
+if __name__ == '__main__':
+    main()
